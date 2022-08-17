9236dc1c4b1f5a33c31d4115fb6fd8a204517f62
Maxim Tresk <djtresk@gmail.com>
2022-08-17 17:54:55 +0500

    change repo

--- deploy.sh
+++ deploy.sh
@@ -20,6 +20,6 @@ git commit -m 'deploy'
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git main
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
-git push -f git@github.com:mTresk/museum-germany.git master:gh-pages
+git push -f git@github.com:mTresk/museum-italy.git master:gh-pages
 
 cd -
